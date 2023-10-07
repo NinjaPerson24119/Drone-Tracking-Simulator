e3ef9de943461c01d4f7f3e0fab2dc18dcc67423
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-06 21:54:11 -0600

    wording: tracker -> device

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,22 @@
 CREATE extension IF NOT EXISTS "uuid-ossp";
 
-CREATE SCHEMA tracker;
+CREATE SCHEMA device;
 
-CREATE TABLE IF NOT EXISTS tracker.device (
-    tracker_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    tracker_name TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS device.information (
+    device_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    device_name TEXT NOT NULL,
     created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     deleted TIMESTAMPTZ
 );
 
-CREATE TABLE IF NOT EXISTS tracker.geolocation (
-    tracker_id uuid REFERENCES tracker.device NOT NULL,
+CREATE TABLE IF NOT EXISTS device.geolocation (
+    device_id uuid REFERENCES device.information NOT NULL,
     event_time TIMESTAMPTZ UNIQUE NOT NULL,
     longitude DECIMAL NOT NULL CHECK(longitude >= -180 AND longitude <= 180),
     latitude DECIMAL NOT NULL CHECK(latitude >= -90 AND latitude <= 90),
     created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     deleted TIMESTAMPTZ,
-    PRIMARY KEY (tracker_id, event_time)
+    PRIMARY KEY (device_id, event_time)
 );
