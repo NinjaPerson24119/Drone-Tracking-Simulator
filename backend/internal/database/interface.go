5212f238e9d4d68b9bf0fdecb461f748434d4c8b
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-06 22:01:59 -0600

    feat: insert commands

--- backend/internal/database/interface.go
+++ backend/internal/database/interface.go
@@ -2,9 +2,11 @@ package database
 
 import (
 	"context"
+	"time"
 )
 
 type Repo interface {
-	InsertDevice(ctx context.Context, name string) (id string, err error)
-	AddGeolocation(ctx context.Context, deviceID string, latitude float64, longitude float64) error
+	InsertDevice(ctx context.Context, name string) (string, error)
+	InsertGeolocation(ctx context.Context, deviceID string, eventTime time.Time, latitude float64, longitude float64)
+	GetLatestGeolocations(ctx context.Context, deviceID string) (string, time.Time, float64, float64, error)
 }
--- backend/internal/database/repo.go
+++ backend/internal/database/repo.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"time"
 
 	//"github.com/jackc/pgx/v5"
 	//"github.com/jackc/pgtype"
@@ -24,15 +25,30 @@ func New(ctx context.Context, connectionURL string) (*RepoImpl, error) {
 	}, nil
 }
 
-func (s *RepoImpl) InsertDevice(ctx context.Context, name string) (id string, err error) {
-	//tx, err := s.pool.Query(ctx)
-	//if err != nil {
-	//	return "", err
-	//}
-
-	return "", nil
+func (s *RepoImpl) InsertDevice(ctx context.Context, name string) (string, error) {
+	var id string
+	err := s.pool.QueryRow(ctx, `
+		INSERT INTO device.information (device_name) VALUES ($1) RETURNING device_id;
+	`, name).Scan(&id)
+	if err != nil {
+		return "", fmt.Errorf("failed to insert device: %v", err)
+	}
+	return id, nil
 }
 
-func (s *RepoImpl) AddGeolocation(ctx context.Context, deviceID string, latitude float64, longitude float64) error {
+func (s *RepoImpl) InsertGeolocation(ctx context.Context, deviceID string, eventTime time.Time, latitude float64, longitude float64) error {
+	tag, err := s.pool.Exec(ctx, `
+		INSERT INTO device.geolocation (device_id, latitude, longitude) VALUES ($1, $2, $3);
+	`, deviceID, eventTime, latitude, longitude)
+	if err != nil {
+		return fmt.Errorf("failed to insert geolocation: %v", err)
+	}
+	if tag.RowsAffected() != 1 {
+		return fmt.Errorf("failed to insert geolocation: no rows affected")
+	}
 	return nil
+}
+
+func (s *RepoImpl) GetLatestGeolocations(ctx context.Context, deviceID string) (string, time.Time, float64, float64, error) {
+	return "", time.Time{}, 0, 0, nil
 }
