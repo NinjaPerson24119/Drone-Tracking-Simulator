fddec8900bf89bfb82599906be2e73cd5ca6d5b6
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-08 00:23:55 -0600

    fix websocket closing prematurely, render markers, this is really slow

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,23 @@ import { useRef, useState, useEffect } from 'react';
 import styles from './page.module.css'
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
+const geolocationStreamAPI = 'wss://map-project-backend.onrender.com/geolocation/stream';
+
+interface GeolocationMessage {
+  geolocations: Geolocation[];
+}
+
+interface Geolocation {
+  device_id: string;
+  latitude: number;
+  longitude: number;
+}
+
 export default function Home() {
+  const [geolocations, setGeolocations] = useState<Map<string, Geolocation>>(new Map<string, Geolocation>());
+  const [markers, setMarkers] = useState<Map<string, mapboxgl.Marker>>(new Map<string, mapboxgl.Marker>());
+  const socket = useRef<WebSocket | null>(null);
+
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null);
 
@@ -13,6 +29,7 @@ export default function Home() {
   const [longitude, setLongitude] = useState(-113.5068);
   const [zoom, setZoom] = useState(16.2);
 
+  // initialize map
   useEffect(() => {
     if(map.current) {
       return;
@@ -40,6 +57,70 @@ export default function Home() {
     });
   })
 
+  // connect to websocket and listen to geolocation stream
+  useEffect(() => {
+    if (socket.current) {
+      return;
+    }
+    
+    // TODO: handle retrying connection
+    const ws = new WebSocket(geolocationStreamAPI);
+    ws.addEventListener('open', () => {
+      console.log('WebSocket connection established.');
+    });
+    ws.addEventListener('close', () => {
+      console.log('WebSocket connection closed.');
+    });
+    ws.addEventListener('error', (error) => {
+      console.error('WebSocket error:', error);
+    });
+    ws.addEventListener('message', (event) => {
+      try {
+        // TODO: validate schema
+        const json: GeolocationMessage = JSON.parse(event.data);
+        for (const geolocation of json.geolocations) {
+          geolocations.set(geolocation.device_id, geolocation);
+        }
+        setGeolocations(new Map(geolocations));
+        console.log('Geolocations:', geolocations);
+      } catch (error) {
+        console.error('Error while reading WebSocket message:', error);
+      }
+    });
+    socket.current = ws;
+    return () => {
+      if (ws.readyState === 1) {
+        ws.close();
+      }
+    };
+  })
+
+  // add/update markers
+  useEffect(() => {
+    if (!map.current) {
+      return;
+    }
+
+    for (const geolocation of geolocations.values()) {
+      if (markers.has(geolocation.device_id)) {
+        const marker = markers.get(geolocation.device_id);
+        if (marker) {
+          marker.setLngLat([geolocation.longitude, geolocation.latitude]);
+        }
+        continue;
+      }
+      const marker = new mapboxgl.Marker({
+        color: '#FF0000',
+        draggable: false,
+        anchor: 'center',
+        //element: ReactDOM.render(() => <div className={styles.marker}></div>),
+      }).setLngLat([geolocation.longitude, geolocation.latitude]).addTo(map.current);
+      marker.setLngLat
+    }
+
+    setMarkers(new Map(markers));
+  }, [geolocations])
+
   return (
     <main className={styles.main}>
       <div className={styles.detailsContainer}>
@@ -48,6 +129,7 @@ export default function Home() {
         <p>Latitude: {latitude}</p>
         <p>Longitude: {longitude}</p>
         <p>Zoom: {zoom}</p>
+        <p>{JSON.stringify(geolocations)}</p>
       </div>
       <div ref={mapContainer} className={styles.mapContainer}></div>
     </main>
