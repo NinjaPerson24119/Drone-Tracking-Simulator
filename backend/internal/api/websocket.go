50faf603ed06b8e392d3ba2acdb4eb32b3ce1820
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-09 10:19:26 -0600

    send all geolocations initially

--- backend/internal/api/websocket.go
+++ backend/internal/api/websocket.go
@@ -1,12 +1,14 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/NinjaPerson24119/MapProject/backend/internal/database"
+	"github.com/NinjaPerson24119/MapProject/backend/internal/filters"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
@@ -73,6 +75,25 @@ func geolocationsWebSocketGenerator(repo database.Repo) func(c *gin.Context) {
 			}
 		}()
 
+		// begin connection by sending all geolocations
+		geolocations, err := getLatestGeolocations(c.Request.Context(), repo)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		json := GeolocationsWebSocketMessage{
+			Geolocations: geolocations,
+		}
+		muWriter.Lock()
+		ws.SetWriteDeadline(time.Now().Add(writeWait))
+		err = ws.WriteJSON(json)
+		muWriter.Unlock()
+		if err != nil {
+			fmt.Printf("error writing json to websocket: %v\n", err)
+			return
+		}
+
+		// listen to updates and send new geolocations as they occur
 		err = repo.ListenToGeolocationInserted(c.Request.Context(), func(geolocation *database.DeviceGeolocation) error {
 			json := GeolocationsWebSocketMessage{
 				Geolocations: []*database.DeviceGeolocation{geolocation},
@@ -94,4 +115,22 @@ func geolocationsWebSocketGenerator(repo database.Repo) func(c *gin.Context) {
 			return
 		}
 	}
+}
+
+func getLatestGeolocations(ctx context.Context, repo database.Repo) ([]*database.DeviceGeolocation, error) {
+	geolocations := []*database.DeviceGeolocation{}
+	for {
+		geolocationsPage, err := repo.GetLatestGeolocations(ctx, filters.PageOptions{
+			Page:     1,
+			PageSize: 100,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("error getting latest geolocations: %v\n", err)
+		}
+		if len(geolocationsPage) == 0 {
+			break
+		}
+		geolocations = append(geolocations, geolocationsPage...)
+	}
+	return geolocations, nil
 }
