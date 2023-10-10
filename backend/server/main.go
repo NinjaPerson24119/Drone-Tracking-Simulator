e40cfa591d8c1c25f94333197fa3372bc3e91a49
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-09 18:39:57 -0600

    profiler code

--- backend/server/main.go
+++ backend/server/main.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/NinjaPerson24119/MapProject/backend/internal/api"
 	"github.com/NinjaPerson24119/MapProject/backend/internal/constants"
 	"github.com/NinjaPerson24119/MapProject/backend/internal/database"
 	"github.com/NinjaPerson24119/MapProject/backend/internal/simulator"
 	"github.com/gin-gonic/gin"
+
+	"runtime/pprof"
 )
 
 func setupBaseRouter() *gin.Engine {
@@ -25,8 +29,33 @@ func setupBaseRouter() *gin.Engine {
 
 func main() {
 	ctx := context.Background()
-	connectionURL := os.Getenv("POSTGRES_CONNECTION_URL")
 
+	// profiler
+	cpuProfileFile, err := os.Create("cpu.pprof")
+	if err != nil {
+		os.Exit(-1)
+	}
+	defer cpuProfileFile.Close()
+	pprof.StartCPUProfile(cpuProfileFile)
+	defer pprof.StopCPUProfile()
+	memProfileFile, err := os.Create("mem.pprof")
+	if err != nil {
+		os.Exit(-1)
+	}
+	defer memProfileFile.Close()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		sig := <-sigChan
+		fmt.Printf("Received signal: %v\n", sig)
+		pprof.StopCPUProfile()
+		cpuProfileFile.Close()
+		memProfileFile.Close()
+		os.Exit(0)
+	}()
+
+	connectionURL := os.Getenv("POSTGRES_CONNECTION_URL")
 	repo, err := database.New(ctx, connectionURL)
 	if err != nil {
 		os.Exit(postgresConnectionFailed)
@@ -37,7 +66,7 @@ func main() {
 	// Edmonton legislature
 	latitude := 53.5357
 	longitude := -113.5068
-	simulator := simulator.New(repo, constants.SimulatedDevices, latitude, longitude, 0.25, 30, 0.05)
+	simulator := simulator.New(repo, constants.SimulatedDevices, latitude, longitude, 0.25, 30, 0.025)
 	ctxWithCancel, cancel := context.WithCancel(ctx)
 	defer cancel()
 	go simulator.Run(ctxWithCancel)
